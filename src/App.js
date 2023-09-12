9e366e5b3820ad6b5f09488c2568246e1cbad78e
mishab777 <muhammedmishab002@gmail.com>
2023-09-12 16:09:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import { Navbar } from './Navbar/Navbar';
+import { Home } from './Home/Home';
+import { Products } from './Products/Products';
+import { Single } from './Products/Single';
+import { Shopcontextprovider } from './context/Shop-context';
+import { Cart } from './cart/Cart';
+import Login from './Login/Login';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Shopcontextprovider>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/Products' element={<Products/>}/>
+          <Route path="/products/:productId" Component={Single}/>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/login' element={<Login/>}/>
+      </Routes>
+      </BrowserRouter>
+      </Shopcontextprovider>
+   
     </div>
   );
 }
---
+++ src/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Home.css"
+import { Carousal } from './Carousal'
+import { Item } from './Item'
+import { Link } from 'react-router-dom'
+import { Footer } from './Footer'
+
+export const Home = () => {
+  return (
+    <div>
+        <Carousal/>
+        <Item/>
+        <div className="view">
+        <Link to='Products'><button>View All Products on Sale!</button></Link>
+        </div>
+        <Footer/>
+        
+    </div>
+  )
+}
